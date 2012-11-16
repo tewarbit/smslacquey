e01ee473c7e0835d13cc932d9acdc35b8cad33cf
Benjamin Tewari <btewari@benjamin-tewaris-macbook-pro-4.local>
2012-11-16 09:17:28 -0500

    get it running

--- lib/smslacquey/movietimes/movietimessteward.rb
+++ lib/smslacquey/movietimes/movietimessteward.rb
@@ -1,5 +1,9 @@
 require 'appengine-apis/logger'
 
+require 'lib/smslacquey/movietimes/movietimesparser.rb'
+require 'lib/smslacquey/movietimes/movietimesfilter.rb'
+require 'lib/smslacquey/movietimes/movietimesrenderer.rb'
+
 
 module SmsLacquey
   
@@ -13,9 +17,9 @@ module SmsLacquey
       urlFetch = AppEngine::URLFetch.fetch('http://www.google.com/movies?hl=en&near=48103&dq=showtimes+48103&q=showtimes&sa=X', {:deadline => 60})  
 
       doc = Nokogiri::HTML(urlFetch.body)
-      movie_info = MovieTimesParser.instance.get_movie_times()
+      movie_info = MovieTimesParser.instance.get_movie_times(doc)
       
-      movie_info = MoveTimesFilter.instance.filter_movies(movie_info, msg_body)
+      movie_info = MovieTimesFilter.instance.filter_movies(movie_info, msg_body)
      
      MovieTimesRenderer.instance.render_message(movie_info)
       
